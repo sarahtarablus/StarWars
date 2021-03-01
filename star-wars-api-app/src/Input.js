0edde139f3d547c517973c9d792d619ac20c43e6
sarahTarablus <sarahtarablus@gmail.com>
2021-03-01 15:15:55 -0800

    update

--- star-wars-api-app/src/Input.js
+++ star-wars-api-app/src/Input.js
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class Input extends Component {
   render () {
     return (
-      <div className='d-flex justify-content-center mt-5'>
+      <div className='d-flex justify-content-center m-5'>
         <input onChange={this.props.onChange} onClick={this.props.onClick} className='form-control-sm bg-warning'></input>
       </div>
     )
--- star-wars-api-app/src/StarWarsPage.js
+++ star-wars-api-app/src/StarWarsPage.js
@@ -23,6 +23,7 @@ class StarWarsPage extends Component {
     this.showPage = this.showPage.bind(this);
     this.searchCharacter = this.searchCharacter.bind(this);
     this.resetInputValue = this.resetInputValue.bind(this);
+    
   }
 
   
@@ -62,68 +63,67 @@ class StarWarsPage extends Component {
 
 
 
-
-
   searchCharacter (e) {
     e.preventDefault();
-    let input = this.state.input;
-    if(input === ''){
-      console.log('ops')
+    this.setState({input: e.target.value})
+    if(e.target.value === ''){
+      this.showPage(1)
+      console.log('no')
     }else{
       if(this.state.isLoaded){
-        this.setState({isLoaded: false})
+        this.setState({isLoaded: false,
+                       newCharacters: [],
+                      characters: []})
       }
-      this.getNewCharacters()
+      this.handleInputChange()
     }
   }
-    
 
 
- 
-  getNewCharacters = async () => {
-    try{
-      const page1 = await axios.get('https://swapi.dev/api/people/?page=1').then(res => res.data.results)
-      const page2 = await axios.get('https://swapi.dev/api/people/?page=2').then(res => res.data.results)
-      const page3 = await axios.get('https://swapi.dev/api/people/?page=3').then(res => res.data.results)
-      const page4 = await axios.get('https://swapi.dev/api/people/?page=4').then(res => res.data.results)
-      const page5 = await axios.get('https://swapi.dev/api/people/?page=5').then(res => res.data.results)
-      const page6 = await axios.get('https://swapi.dev/api/people/?page=6').then(res => res.data.results)
-      const page7 = await axios.get('https://swapi.dev/api/people/?page=7').then(res => res.data.results)
-      const page8 = await axios.get('https://swapi.dev/api/people/?page=8').then(res => res.data.results)
-      const page9 = await axios.get('https://swapi.dev/api/people/?page=9').then(res => res.data.results)
-      
-      const pages = page1.concat(page2, page3, page4, page5, page6, page7, page8, page9)
 
-      for(const character of pages){
-        const homeworldURL = character.homeworld
-        const homeworldRes = await axios.get(homeworldURL).then(res => res.data.name)
-        character.homeworld = [homeworldRes]
-        if(character.species.length !== 0){
-         const speciesURL = character.species;
-         const speciesRes = await axios.get(speciesURL).then(res => res.data.name)
-         character.species = [speciesRes]
-        }else{
-          character.species = 'Human'
-        }
+
+ handleInputChange = 
+  async () => {
+      try{
+        const page1 = await axios.get('https://swapi.dev/api/people/?page=1').then(res => res.data.results)
+        const page2 = await axios.get('https://swapi.dev/api/people/?page=2').then(res => res.data.results)
+        const page3 = await axios.get('https://swapi.dev/api/people/?page=3').then(res => res.data.results)
+        const page4 = await axios.get('https://swapi.dev/api/people/?page=4').then(res => res.data.results)
+        const page5 = await axios.get('https://swapi.dev/api/people/?page=5').then(res => res.data.results)
+        const page6 = await axios.get('https://swapi.dev/api/people/?page=6').then(res => res.data.results)
+        const page7 = await axios.get('https://swapi.dev/api/people/?page=7').then(res => res.data.results)
+        const page8 = await axios.get('https://swapi.dev/api/people/?page=8').then(res => res.data.results)
+        const page9 = await axios.get('https://swapi.dev/api/people/?page=9').then(res => res.data.results)
         
-        if(character.name.toLowerCase().indexOf(this.state.input) !== -1){
-              console.log(character)
-              let char = this.state.newCharacters;
-              char.unshift(character)
-              if(char.length > 10){
-                char.pop()
-              }
-              this.setState({newCharacters : char,
-                            characters : this.state.newCharacters,
-                            isLoaded: true
-                            })
-              return;     
-        }  
+    
+        const pages = page1.concat(page2, page3, page4, page5, page6, page7, page8, page9)
+  
+  
+        for(const character of pages) {
+          const homeworldURL = character.homeworld
+          const homeworldRes = await axios.get(homeworldURL).then(res => res.data.name)
+          character.homeworld = [homeworldRes]
+          if(character.species.length !== 0){
+           const speciesURL = character.species;
+           const speciesRes = await axios.get(speciesURL).then(res => res.data.name)
+           character.species = [speciesRes]
+          }else{
+            character.species = 'Human'
+          }
+           
+          if(character.name.toLowerCase().startsWith((this.state.input).toLowerCase()) == true && this.state.input !== ''){
+           
+          let newCharacters = this.state.newCharacters;
+          newCharacters.push(character);
+          const characters = [...new Map(newCharacters.map(char => [char.name, char])).values()]
+          this.setState({characters: [...characters]})
+        }
+      }   
+        this.setState({isLoaded: true})
+      }catch(err) {
+        console.error(err)
       }
-    } catch(err) {
-      console.error(err)
-    }
-  } 
+ }
 
 
 
@@ -136,16 +136,6 @@ class StarWarsPage extends Component {
     this.showPage(e.target.value)
   }
 
-
-
-
-  handleInputChange (e) {
-    e.preventDefault();
-    let inputValue = e.target.value;
-    this.setState({
-      input : inputValue 
-    })
-  }
 
 
 
@@ -163,13 +153,13 @@ class StarWarsPage extends Component {
       <div>
         <div className='text-warning mt-5 w-75 border border-white rounded mx-auto'>
           <h1 className='d-flex justify-content-center display-3'>STAR WARS</h1>
-          <form className='form' onSubmit={this.searchCharacter}>
-           <Input onChange={this.handleInputChange} onClick={this.resetInputValue}/>
-           <SearchButton />
+          <form className='form'>
+           <Input onChange={this.searchCharacter} onClick={this.resetInputValue}/>
+           {/* <SearchButton /> */}
           </form>
         </div>
         <div className='text-white'>
-          {isLoaded ?  <Table data={this.state.characters.map((char, i) => {
+          {isLoaded ? <Table data={this.state.characters.map((char, i) => {
             return(
               <tr key={i}>
                 <td>{char.name}</td>
@@ -179,8 +169,7 @@ class StarWarsPage extends Component {
                 <td>{char.homeworld}</td>
                 <td>{char.species}</td>
               </tr>
-         )})}/> : <div className='d-flex justify-content-center m-5'>Loading...</div>}
-        
+         )})}/> : <div className='d-flex justify-content-center m-5'>Loading...</div>} 
        </div>
        <div>
          <ButtonPageChange onClick={this.changePageCount} ></ButtonPageChange>
@@ -190,6 +179,5 @@ class StarWarsPage extends Component {
   }
  }  
 
-
-
+  
 export default StarWarsPage;
